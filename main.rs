daf25e7539748b25d905c51520d0d24a858c16f9
Six <151474844+zxhenrii@users.noreply.github.com>
2025-04-07 13:33:56 -0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,1 +1,10 @@
 
+fn main() {
+    for number in 1..=10 {
+        if number % 2 == 0 {
+            println!("{} é par", number);
+        } else {
+            println!("{} é ímpar", number);
+        }
+    }
+}
